3755546a2ac1e2a9c5440d274736b0bf13a5a88a
zeropointnothing <ZeroPointNothing@outlook.com>
2025-12-14 18:54:44 -0500

    fix flag detection
    Whoops! We probably should also check 'cswitch' too!

--- koulouri_shared/cmdparser.cpp
+++ koulouri_shared/cmdparser.cpp
@@ -27,7 +27,7 @@ ParseResult CmdParser::parse_args(int argc, char* argv[]) {
     std::vector<ArgResult> final;
     for (Argument &arg : registered) {
         for (int i=1; i<argc; i++) {
-            if (strcmp(argv[i], arg.cswitch_long)==0 || strcmp(argv[i], arg.cswitch_long)==0) {
+            if (strcmp(argv[i], arg.cswitch)==0 || strcmp(argv[i], arg.cswitch_long)==0) {
                 if (arg.type == ArgType::SWITCH) {
                     final.push_back({arg, true});
                 } else if (arg.type == ArgType::VALUE) {
